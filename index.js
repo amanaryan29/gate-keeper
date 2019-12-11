414d3b57b378c3af78ce401a9a79ec94fcda9a48
Aman Aryan <aman.a@scroll.in>
2019-12-11 18:47:50 +0530

    interceptor code added

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-module.exports.openGateKeeper(baseURL, loginURL, cookieName) {
-    const getCookie = key => Cookies.get(key);
-    const Authorization = getCookie(cookieName);
-    let headers = {};
-    if (Authorization) {
-        headers.Authorization = Authorization;
-    }
-
-    const apiClient = axios.create({
-        baseURL: baseURL,
-        timeout: 10000,
-        headers
-    });
-
-    apiClient.interceptors.response.use(
-        response => {
-            return response;
-        },
-        error => {
-            if (error.response.status === 401) {
-                window.location = loginURL;
-            } else {
-                return error;
-            }
-        }
-    );
+const config = {
+    baseURL: '',
+    loginURL: '',
+    cookieName: ''
 }
+
+function onError() {
+    window.location = config.loginURL;
+}
+
+const getCookie = key => Cookies.get(key);
+const Authorization = getCookie(config.cookieName);
+let headers = {};
+
+if (Authorization) {
+    headers.Authorization = Authorization;
+} else {
+    onError()
+}
+
+
+const apiClient = axios.create({
+    baseURL: config.baseURL,
+    timeout: 10000,
+    headers
+});
+
+
+apiClient.interceptors.response.use(
+    response => {
+        return response.data;
+    },
+    error => {
+        if (error.response.status > 400) {
+            onError();
+        } else {
+            return error;
+        }
+    }
+);
+
+
+function setData(baseURL, loginURL, cookieName) {
+    config.baseURL = baseURL;
+    config.loginURL = loginURL;
+    config.cookieName = cookieName;
+}
+
+apiClient.setData = setData
+
+export default apiClient
